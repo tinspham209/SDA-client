f4784bf576e18eb0a1825d8758a29c1fc499c762
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-12 12:36:49 +0700

    setup route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar } from "./components";
+import GlobalStyle from "./GlobalStyles";
+
+import { NotFound, Spinner } from "./components/UI/";
+import ScrollToTop from "./components/ScrollToTop";
+const HomePage = React.lazy(() => import("./pages/HomePage/HomePage"));
 
 function App() {
 	return (
-		<div className="app">
-			<h2>Hello world</h2>
-		</div>
+		<Suspense fallback={<Spinner />}>
+			<Router>
+				<GlobalStyle />
+				<ScrollToTop />
+				<Navbar />
+				<Switch>
+					<Route path="/" exact component={HomePage} />
+					<Route component={NotFound} />
+				</Switch>
+			</Router>
+		</Suspense>
 	);
 }
 
