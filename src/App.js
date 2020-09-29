8c78ac36a8288e355cfae88e0fd8c42cf4e22ee9
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-29 11:02:17 +0700

    update navbar dashboard with redux

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 import React, { Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Navbar, Footer } from "./components";
-import GlobalStyle from "./GlobalStyles";
 
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+import GlobalStyle from "./GlobalStyles";
+import { Navbar, Footer, NavbarDashboard } from "./components";
 import { NotFound, Spinner } from "./components/UI/";
 import ScrollToTop from "./components/ScrollToTop";
+
+import { useSelector } from "react-redux";
+
 const HomePage = React.lazy(() => import("./pages/HomePage/HomePage"));
 const About = React.lazy(() => import("./pages/About/About"));
 const Dashboard = React.lazy(() => import("./pages/Dashboard/Dashboard"));
 const Contact = React.lazy(() => import("./pages/Contact/Contact"));
 
 function App() {
+	const isDashboard = useSelector((state) => state.dashboard.isDashboard);
+
 	return (
 		<Suspense fallback={<Spinner />}>
 			<Router>
-				<GlobalStyle />
-				<ScrollToTop />
-				<Navbar />
-				<Switch>
-					<Route path="/" exact component={HomePage} />
-					<Route path="/about" exact component={About} />
-					<Route path="/dashboard" exact component={Dashboard} />
-					<Route path="/contact" exact component={Contact} />
-					<Route component={NotFound} />
-				</Switch>
-				<Footer />
+				<DndProvider backend={HTML5Backend}>
+					<GlobalStyle />
+					<ScrollToTop />
+					{isDashboard ? <NavbarDashboard /> : <Navbar />}
+					<Switch>
+						<Route path="/" exact component={HomePage} />
+						<Route path="/about" exact component={About} />
+						<Route path="/dashboard" exact component={Dashboard} />
+						<Route path="/contact" exact component={Contact} />
+						<Route component={NotFound} />
+					</Switch>
+					<Footer />
+				</DndProvider>
 			</Router>
 		</Suspense>
 	);
