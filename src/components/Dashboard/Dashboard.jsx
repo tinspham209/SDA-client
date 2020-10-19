d9100af57b9954dcdb9b6b41997d7709aa1e52d8
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-10-20 00:07:56 +0700

    dashboard component

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import {
+	DashboardContainer,
+	GridContainer,
+	Layout,
+} from "./Dashboard.elements";
+import { Toolbar } from "./index";
+const Dashboard = () => {
+	return (
+		<Layout>
+			<DashboardContainer>
+				<GridContainer container spacing={1}>
+					<GridContainer item xs={2}>
+						<Toolbar />
+					</GridContainer>
+					<GridContainer item xs={8}>
+						<div style={{ border: "1px solid red" }}></div>
+					</GridContainer>
+					<GridContainer item xs>
+						<div style={{ border: "1px solid red" }}></div>
+					</GridContainer>
+				</GridContainer>
+			</DashboardContainer>
+		</Layout>
+	);
+};
+
+export default Dashboard;
