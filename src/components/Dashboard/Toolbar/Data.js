7cba2cd6384dbc7e4fe1524e8a4e27c469bea48a
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-10-27 21:57:34 +0700

    initial items data for toolbar

---
+++ src/components/Dashboard/Toolbar/Data.js
@@ -0,0 +1,104 @@
+export const listItems = [
+	{
+		id: "climate",
+		name: "Climate",
+		collapseItem: [
+			{
+				id: "avg-temperature",
+				name: "Average Temperature",
+			},
+			{
+				id: "avg-precipitation",
+				name: "Average Precipitation",
+			},
+			{
+				id: "avg-rainfall",
+				name: "Average Rainfall",
+			},
+		],
+	},
+	{
+		id: "atmosphere",
+		name: "Atmosphere",
+		collapseItem: [
+			{
+				id: "air-quality-station",
+				name: "Air Quality Station",
+			},
+			{
+				id: "air-quality-filter",
+				name: "Air Quality Filter",
+			},
+		],
+	},
+	{
+		id: "population",
+		name: "Population",
+		collapseItem: [
+			{
+				id: "populationItem",
+				name: "Population",
+			},
+		],
+	},
+	{
+		id: "industry",
+		name: "Industry",
+		collapseItem: [
+			{
+				id: "industryItem",
+				name: "Industry Production",
+			},
+		],
+	},
+	{
+		id: "forest",
+		name: "Forest",
+		collapseItem: [
+			{
+				id: "forest-cover-area",
+				name: "Forest Cover Area",
+			},
+			{
+				id: "afForestation",
+				name: "AfForestation",
+			},
+		],
+	},
+	{
+		id: "operators",
+		name: "Operators",
+		collapseItem: [
+			{
+				id: "statistics-merge",
+				name: "Statistics Merge",
+			},
+			{
+				id: "simple-merge",
+				name: "Simple Merge",
+			},
+		],
+	},
+	{
+		id: "visualization",
+		name: "Visualization",
+		collapseItem: [
+			{
+				id: "column-chart",
+				name: "Column Chart",
+			},
+			{
+				id: "line-chart",
+				name: "Line Chart",
+			},
+			{
+				id: "pie-chart",
+				name: "Pie Chart",
+			},
+			{
+				id: "maps-viz",
+				name: "Maps",
+			},
+		],
+	},
+];
