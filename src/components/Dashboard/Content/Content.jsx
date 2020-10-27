8148ecb7c4210d9ae04765e8f6d52a9fff2c5b3a
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-10-27 23:13:04 +0700

    addNewNode & removeNode

--- src/components/Dashboard/Content/Content.jsx
+++ src/components/Dashboard/Content/Content.jsx
@@ -1,55 +1,79 @@
 import React from "react";
-import { CustomContent } from "./Content.elements";
+import { useDrop } from "react-dnd";
+import { ItemTypes } from "../../../app/ItemTypes";
 
 import Diagram, { useSchema } from "beautiful-react-diagrams";
 import "beautiful-react-diagrams/styles.css";
 
+import { CustomContent } from "./Content.elements";
+import { Button } from "../../../GlobalStyles";
+
 const initialSchema = {
-	nodes: [
-		{
-			id: "node-1",
-			content: "Start",
-			coordinates: [100, 150],
-			outputs: [{ id: "port-1", alignment: "right" }],
-			data: {
-				foo: "bar",
-				count: 0,
-			},
-		},
-		{
-			id: "node-2",
-			content: "Middle",
-			coordinates: [300, 150],
-			inputs: [{ id: "port-3", alignment: "left" }],
-			outputs: [{ id: "port-5", alignment: "right" }],
-			data: {
-				bar: "foo",
-			},
-		},
-		{
-			id: "node-3",
-			content: "End",
-			coordinates: [600, 150],
-			inputs: [{ id: "port-7", alignment: "left" }],
-			data: {
-				foo: true,
-				bar: false,
-				some: {
-					deep: {
-						object: true,
-					},
-				},
-			},
-		},
-	],
-	links: [{ input: "port-1", output: "port-3" }],
+	nodes: [],
 };
 
+const CustomRender = ({ id, content, data, inputs, outputs }) => (
+	<div style={{ background: "purple" }}>
+		<div style={{ textAlign: "right" }}>
+			<Button onClick={() => data.onClick(id)}>x </Button>
+		</div>
+		<div role="button" style={{ padding: "15px" }}>
+			{content}
+		</div>
+		<div
+			style={{
+				marginTop: "10px",
+				display: "flex",
+				justifyContent: "space-between",
+			}}
+		>
+			{inputs.map((port) =>
+				React.cloneElement(port, {
+					style: { width: "25px", height: "25px", background: "#1B263B" },
+				})
+			)}
+			{outputs.map((port) =>
+				React.cloneElement(port, {
+					style: { width: "25px", height: "25px", background: "#1B263B" },
+				})
+			)}
+		</div>
+	</div>
+);
+
 const Content = () => {
-	const [schema, { onChange }] = useSchema(initialSchema);
+	const [schema, { onChange, addNode, removeNode }] = useSchema(initialSchema);
+
+	// dnd from list to container
+	const [, dropList] = useDrop({
+		accept: ItemTypes.ITEM,
+		drop: (item, monitor) => {
+			addNewNode(monitor.getClientOffset().x, monitor.getClientOffset().y);
+			return undefined;
+		},
+	});
+
+	const deleteNodeFromSchema = (id) => {
+		const nodeToRemove = schema.nodes.find((node) => node.id === id);
+		removeNode(nodeToRemove);
+	};
+
+	const addNewNode = (x, y) => {
+		const nextNode = {
+			id: `node-${schema.nodes.length + 1}`,
+			content: `Node ${schema.nodes.length + 1}`,
+			coordinates: [x - 250, y - 70],
+			render: CustomRender,
+			data: { onClick: deleteNodeFromSchema },
+			inputs: [{ id: `port-${Math.random()}` }],
+			outputs: [{ id: `port-${Math.random()}` }],
+		};
+		addNode(nextNode);
+	};
+
 	return (
 		<>
-			<CustomContent>
+			<CustomContent ref={dropList}>
 				<Diagram schema={schema} onChange={onChange} />
 			</CustomContent>
 		</>
