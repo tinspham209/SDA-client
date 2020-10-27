ccb034814e5e1d5e7f808d6ecd7e3fca16d17f82
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-10-27 23:13:35 +0700

    content state

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import dashboardReducer from "../pages/Dashboard/dashboardSlice";
 import toolbarReducer from "../components/Dashboard/Toolbar/ToolbarSlice";
+import contentReducer from "../components/Dashboard/Content/ContentSlice";
 
 const rootReducer = {
 	dashboard: dashboardReducer,
 	toolbar: toolbarReducer,
+	content: contentReducer,
 };
 
 const store = configureStore({
