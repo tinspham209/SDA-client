f9380650e661b700a10d62aae3a054cb4e06b0c8
tinspham209 <tindeptrai1@gmail.com>
2020-11-05 23:59:29 +0700

    Viz Table TreeView in Node

--- src/components/Dashboard/Content/Content.jsx
+++ src/components/Dashboard/Content/Content.jsx
@@ -6,40 +6,11 @@ import Diagram, { useSchema } from "beautiful-react-diagrams";
 import "beautiful-react-diagrams/styles.css";
 
 import { CustomContent } from "./Content.elements";
-import { Button } from "../../../GlobalStyles";
+import CustomNode from "../CustomNode/CustomNode";
 
 const initialSchema = {
 	nodes: [],
 };
-
-const CustomRender = ({ id, content, data, inputs, outputs }) => (
-	<div style={{ background: "purple" }}>
-		<div style={{ textAlign: "right" }}>
-			<Button onClick={() => data.onClick(id)}>x </Button>
-		</div>
-		<div role="button" style={{ padding: "15px" }}>
-			{content}
-		</div>
-		<div
-			style={{
-				marginTop: "10px",
-				display: "flex",
-				justifyContent: "space-between",
-			}}
-		>
-			{inputs.map((port) =>
-				React.cloneElement(port, {
-					style: { width: "25px", height: "25px", background: "#1B263B" },
-				})
-			)}
-			{outputs.map((port) =>
-				React.cloneElement(port, {
-					style: { width: "25px", height: "25px", background: "#1B263B" },
-				})
-			)}
-		</div>
-	</div>
-);
 
 const Content = () => {
 	const [schema, { onChange, addNode, removeNode }] = useSchema(initialSchema);
@@ -61,9 +32,8 @@ const Content = () => {
 	const addNewNode = (x, y) => {
 		const nextNode = {
 			id: `node-${schema.nodes.length + 1}`,
-			content: `Node ${schema.nodes.length + 1}`,
 			coordinates: [x - 250, y - 70],
-			render: CustomRender,
+			render: CustomNode,
 			data: { onClick: deleteNodeFromSchema },
 			inputs: [{ id: `port-${Math.random()}` }],
 			outputs: [{ id: `port-${Math.random()}` }],
