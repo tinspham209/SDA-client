2bb4f1c78d4f5e305c1cae26d7b0cb6bd41d9900
tinspham209 <tindeptrai1@gmail.com>
2020-11-06 10:36:29 +0700

    data of treeIndustry

---
+++ src/components/Dashboard/Content/Data.js
@@ -0,0 +1,43 @@
+export const treeIndustry = {
+	name: "Industry Production",
+	data: [
+		{
+			id: "industry",
+			name: "Industry",
+			children: [
+				{
+					id: "industry-2012",
+					name: "2012",
+				},
+				{
+					id: "industry-2013",
+					name: "2013",
+				},
+				{
+					id: "industry-2014",
+					name: "2014",
+				},
+				{
+					id: "industry-2015",
+					name: "2015",
+				},
+				{
+					id: "industry-2016",
+					name: "2016",
+				},
+				{
+					id: "industry-2017",
+					name: "2017",
+				},
+				{
+					id: "industry-2018",
+					name: "2018",
+				},
+				{
+					id: "industry-2019",
+					name: "2019",
+				},
+			],
+		},
+	],
+};
