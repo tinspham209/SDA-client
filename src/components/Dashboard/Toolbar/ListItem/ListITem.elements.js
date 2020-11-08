6bf6ffdabd92ceba8e997f609c2086d79f99d957
tinspham209 <tindeptrai1@gmail.com>
2020-11-08 12:57:38 +0700

    set state isDragItem unique ID when drag item

---
+++ src/components/Dashboard/Toolbar/ListItem/ListITem.elements.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+export const CustomListItem = styled(ListItem)`
+	padding-left: 16px !important;
+	border-bottom: 1px solid rgba(0, 0, 0, 0.2) !important;
+`;
+
+export const CustomListItemText = styled(ListItemText)``;
--- src/components/Dashboard/Toolbar/ListItem/ListItem.jsx
+++ src/components/Dashboard/Toolbar/ListItem/ListItem.jsx
@@ -1,15 +1,39 @@
 import React from "react";
-import { CustomListItem, CustomListItemText } from "../Toolbar.elements";
+
+import { CustomListItem, CustomListItemText } from "./ListITem.elements";
+
 import { useDrag } from "react-dnd";
+import { useDispatch } from "react-redux";
+
 import { ITEM } from "../../../../app/ItemTypes";
+import { setIsDragItem } from "../../../../app/slice/ToolbarSlice";
 
 const ListItem = ({ id, primary }) => {
+	const dispatch = useDispatch();
+
 	const [, drag] = useDrag({
-		item: { type: ITEM },
+		item: { id, type: ITEM },
 		collect: (monitor) => ({
 			isDragging: monitor.isDragging(),
 		}),
+		begin: () => {
+			dragging(id);
+		},
+		end: () => {
+			isDrop(id);
+		},
 	});
+
+	const dragging = (id) => {
+		const action = setIsDragItem(id);
+		dispatch(action);
+	};
+
+	const isDrop = (id) => {
+		const action = setIsDragItem("");
+		dispatch(action);
+	};
+
 	return (
 		<CustomListItem button id={id} ref={drag}>
 			<CustomListItemText primary={primary} />
