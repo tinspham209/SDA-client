7cec3ae8fdc42a4daa373744f87d7e41c9c1266b
tinspham209 <tindeptrai1@gmail.com>
2020-11-08 12:59:16 +0700

    render Industry

--- src/components/Dashboard/CustomNode/CustomNode.jsx
+++ src/components/Dashboard/CustomNode/CustomNode.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import IconButton from "@material-ui/core/IconButton";
 import { AiFillCloseCircle } from "react-icons/ai";
 
-import TableTree from "../TableTree/TableTree";
-
 import {
 	Body,
 	Header,
@@ -16,52 +14,54 @@ import {
 	PortOut,
 } from "./CustomNode.elements";
 
-const CustomNode = ({ id, data, inputs, outputs }) => (
-	<Node>
-		<Header>
-			<HeaderLeft />
-			<HeaderCenter>
-				<HeaderTitle>Industry Production</HeaderTitle>
-			</HeaderCenter>
-			<HeaderRight>
-				<IconButton
-					aria-label="delete"
-					size="small"
-					color="inherit"
-					onClick={() => data.onClick(id)}
-				>
-					<AiFillCloseCircle />
-				</IconButton>
-			</HeaderRight>
-		</Header>
-		<Body>
-			<TableTree />
-			<PortIn>
-				{inputs.map((port) =>
-					React.cloneElement(port, {
-						style: {
-							width: "15px",
-							height: "15px",
-							background: "#1B263B",
-							borderRadius: "50%",
-						},
-					})
-				)}
-			</PortIn>
-			<PortOut>
-				{outputs.map((port) =>
-					React.cloneElement(port, {
-						style: {
-							width: "15px",
-							height: "15px",
-							background: "#1B263B",
-							borderRadius: "50%",
-						},
-					})
-				)}
-			</PortOut>
-		</Body>
-	</Node>
-);
+const CustomNode = ({ id, data, inputs, outputs }) => {
+	return (
+		<Node>
+			<Header>
+				<HeaderLeft />
+				<HeaderCenter>
+					<HeaderTitle>Industry Production</HeaderTitle>
+				</HeaderCenter>
+				<HeaderRight>
+					<IconButton
+						aria-label="delete"
+						size="small"
+						color="inherit"
+						onClick={() => data.onClick(id)}
+					>
+						<AiFillCloseCircle />
+					</IconButton>
+				</HeaderRight>
+			</Header>
+			<Body>
+				{data.renderNode(id)}
+				<PortIn>
+					{inputs.map((port) =>
+						React.cloneElement(port, {
+							style: {
+								width: "15px",
+								height: "15px",
+								background: "#1B263B",
+								borderRadius: "50%",
+							},
+						})
+					)}
+				</PortIn>
+				<PortOut>
+					{outputs.map((port) =>
+						React.cloneElement(port, {
+							style: {
+								width: "15px",
+								height: "15px",
+								background: "#1B263B",
+								borderRadius: "50%",
+							},
+						})
+					)}
+				</PortOut>
+			</Body>
+		</Node>
+	);
+};
 
 export default CustomNode;
