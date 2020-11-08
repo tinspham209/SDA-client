5ead1b2589bf7cbc25b7f4299c856f96c3fc0638
tinspham209 <tindeptrai1@gmail.com>
2020-11-08 12:59:06 +0700

    addNewNode render industry

--- src/components/Dashboard/Content/Content.jsx
+++ src/components/Dashboard/Content/Content.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { useDrop } from "react-dnd";
-import { ITEM } from "../../../app/ItemTypes";
+import { useSelector } from "react-redux";
+import { ITEM, INDUSTRY_ITEM } from "../../../app/ItemTypes";
 
 import Diagram, { useSchema } from "beautiful-react-diagrams";
 import "beautiful-react-diagrams/styles.css";
 
 import { CustomContent } from "./Content.elements";
 import CustomNode from "../CustomNode/CustomNode";
+import TableTree from "../TableTree/TableTree";
 
 const initialSchema = {
 	nodes: [],
 };
 
 const Content = () => {
+	const isDropItem = useSelector((state) => state.toolbar.isDragItem);
+
 	const [schema, { onChange, addNode, removeNode }] = useSchema(initialSchema);
 
 	// dnd from list to container
@@ -29,12 +33,23 @@ const Content = () => {
 		removeNode(nodeToRemove);
 	};
 
+	const setItemRenderNode = (id) => {
+		// split from "industryItem-1" to "industry"
+		switch (id.split("-")[0]) {
+			case INDUSTRY_ITEM:
+				return <TableTree />;
+			default:
+				return null;
+		}
+	};
+
 	const addNewNode = (x, y) => {
 		const nextNode = {
-			id: `node-${schema.nodes.length + 1}`,
+			//
+			id: `${isDropItem}-${schema.nodes.length + 1}`,
 			coordinates: [x - 250, y - 70],
 			render: CustomNode,
-			data: { onClick: deleteNodeFromSchema },
+			data: { onClick: deleteNodeFromSchema, renderNode: setItemRenderNode },
 			inputs: [{ id: `port-${Math.random()}` }],
 			outputs: [{ id: `port-${Math.random()}` }],
 		};
