20b13326f1389d5321a7c85aca1a0486e6e9fbb6
tinspham209 <tindeptrai1@gmail.com>
2020-11-13 11:42:46 +0700

    unique data TableTree

--- src/components/Dashboard/TableTree/TableTree.jsx
+++ src/components/Dashboard/TableTree/TableTree.jsx
@@ -35,16 +35,19 @@ const TableTree = ({ treeItem }) => {
 			onNodeToggle={handleToggle}
 			onNodeSelect={handleSelect}
 		>
-			<TreeItem nodeId="1" label="Industry">
-				<TreeItem nodeId="2" label="2012" />
-				<TreeItem nodeId="3" label="2013" />
-				<TreeItem nodeId="4" label="2014" />
-				<TreeItem nodeId="5" label="2015" />
-				<TreeItem nodeId="6" label="2016" />
-				<TreeItem nodeId="7" label="2017" />
-				<TreeItem nodeId="8" label="2018" />
-				<TreeItem nodeId="9" label="2019" />
-			</TreeItem>
+			{treeItem.data.map((item) => (
+				<TreeItem nodeId={item.id} label={item.name} key={item.id}>
+					{item.children
+						? item.children.map((children) => (
+								<TreeItem
+									nodeId={children.id}
+									label={children.name}
+									key={children.id}
+								/>
+						  ))
+						: null}
+				</TreeItem>
+			))}
 		</TreeView>
 	);
 };
