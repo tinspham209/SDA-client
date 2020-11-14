ddfe24e0f91cb23c3e03dc1a892ffdf9d1cef0d4
tinspham209 <tindeptrai1@gmail.com>
2020-11-15 02:01:02 +0700

    export components viz

---
+++ src/components/Dashboard/Visualization/index.js
@@ -0,0 +1,4 @@
+export { default as TableTree } from "./TableTree/TableTree";
+export { default as ColumnChart } from "./ColumnChart/ColumnChart";
+export { default as LineChart } from "./LineChart/LineChart";
+export { default as Maps } from "./Maps/Maps";
