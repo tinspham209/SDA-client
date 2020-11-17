61c768a18b3e8a9309feff6b2343422deafa65da
tinspham209 <tindeptrai1@gmail.com>
2020-11-17 23:43:42 +0700

    add: CLIMATE_HUMIDITY

--- src/app/ItemTypes.js
+++ src/app/ItemTypes.js
@@ -3,3 +3,5 @@ export const ITEM = "ITEM";
 export const CONTENT = "CONTENT";
 export const DASHBOARD = "DASHBOARD";
 export const TOOLBAR = "TOOLBAR";
+
+export const CLIMATE_HUMIDITY = "avgHumidity";
