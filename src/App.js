31b69235628e27986cfbce500984d5f2dc988ac0
tinspham209 <tindeptrai1@gmail.com>
2020-11-17 17:57:57 +0700

    remove footer

--- src/App.js
+++ src/App.js
@@ -5,11 +5,8 @@ import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
 import GlobalStyle from "./GlobalStyles";
-import { Footer } from "./components";
 import { NotFound, Spinner } from "./components/UI/";
 import ScrollToTop from "./components/ScrollToTop";
-
-import { useSelector } from "react-redux";
 
 const HomePage = React.lazy(() => import("./pages/HomePage/HomePage"));
 const About = React.lazy(() => import("./pages/About/About"));
@@ -17,8 +14,6 @@ const Dashboard = React.lazy(() => import("./pages/Dashboard/Dashboard"));
 const Contact = React.lazy(() => import("./pages/Contact/Contact"));
 
 function App() {
-	const isDashboard = useSelector((state) => state.dashboard.isDashboard);
-
 	return (
 		<Suspense fallback={<Spinner />}>
 			<Router>
@@ -32,7 +27,6 @@ function App() {
 						<Route path="/contact" exact component={Contact} />
 						<Route component={NotFound} />
 					</Switch>
-					{isDashboard ? null : <Footer />}
 				</DndProvider>
 			</Router>
 		</Suspense>
