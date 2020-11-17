dd70cfb3d762b6ed90b6acf248fbd66dc1f3cb88
tinspham209 <tindeptrai1@gmail.com>
2020-11-17 23:43:07 +0700

    add: climate/humidity widget

--- src/containers/MashupContent/MashupContent.jsx
+++ src/containers/MashupContent/MashupContent.jsx
@@ -1,10 +1,64 @@
 import React from "react";
 import { useStyles } from "./MashupContent.elements";
+
+import Diagram, { useSchema } from "beautiful-react-diagrams";
+import "beautiful-react-diagrams/styles.css";
+
+import { useDrop } from "react-dnd";
+import { useSelector } from "react-redux";
+import { CLIMATE_HUMIDITY, ITEM } from "../../app/ItemTypes";
+
+import { Humidity } from "../../components/Widgets";
+
+const initialSchema = {
+	nodes: [],
+};
+
 const MashupContent = () => {
 	const classes = useStyles();
+
+	const [schema, { onChange, addNode, removeNode }] = useSchema(initialSchema);
+	const isDropItem = useSelector((state) => state.dashboard.toolbar.isDragItem);
+
+	// dnd from list to container
+	const [, dropList] = useDrop({
+		accept: ITEM,
+		drop: (item, monitor) => {
+			addNewNode(monitor.getClientOffset().x, monitor.getClientOffset().y);
+			return undefined;
+		},
+	});
+
+	const getWidget = (id) => {
+		switch (id) {
+			case CLIMATE_HUMIDITY:
+				return Humidity;
+			default:
+				break;
+		}
+	};
+
+	const deleteNodeFromSchema = (id) => {
+		const nodeToRemove = schema.nodes.find((node) => node.id === id);
+		removeNode(nodeToRemove);
+	};
+
+	const addNewNode = (x, y) => {
+		const nextNode = {
+			id: `${isDropItem}-${schema.nodes.length + 1}`,
+			coordinates: [x - 200, y - 70],
+			render: getWidget(isDropItem),
+			data: { onClick: deleteNodeFromSchema },
+			inputs: [{ id: `port-${Math.random()}` }],
+			outputs: [{ id: `port-${Math.random()}` }],
+		};
+
+		addNode(nextNode);
+	};
+
 	return (
-		<div className={classes.mashupContent}>
-			<h2>Content</h2>
+		<div className={classes.mashupContent} ref={dropList}>
+			<Diagram schema={schema} onChange={onChange} />
 		</div>
 	);
 };
