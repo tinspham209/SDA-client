33dceefc22c9811387597fc0817c171e1da9c392
tinspham209 <tindeptrai1@gmail.com>
2020-11-18 22:30:09 +0700

    add: output Log component

--- src/components/OutputLog/OutputLog.elements.js
+++ src/components/OutputLog/OutputLog.elements.js
@@ -3,5 +3,12 @@ import { makeStyles } from "@material-ui/core/styles";
 export const useStyles = makeStyles((theme) => ({
 	outputLog: {
 		width: "100%",
+		overflowY: "auto",
+
+		marginTop: "10px",
+		marginBottom: "10px",
+	},
+	button: {
+		marginBottom: "10px",
 	},
 }));
