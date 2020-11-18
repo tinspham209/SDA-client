af8e8c7e2638ddc062f05f56ca65826981b191f0
tinspham209 <tindeptrai1@gmail.com>
2020-11-18 11:46:48 +0700

    add: WidgetInfo component

--- src/components/WidgetInfo/WidgetInfo.elements.js
+++ src/components/WidgetInfo/WidgetInfo.elements.js
@@ -3,5 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 export const useStyles = makeStyles((theme) => ({
 	widgetInfo: {
 		width: "100%",
+		marginTop: "10px",
+		marginBottom: "10px",
 	},
 }));
