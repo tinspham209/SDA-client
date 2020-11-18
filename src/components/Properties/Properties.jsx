02255a31ba0b9de7e0026f0ca5fa1f12b9867113
tinspham209 <tindeptrai1@gmail.com>
2020-11-18 10:46:51 +0700

    add: properties component

--- src/components/Properties/Properties.jsx
+++ src/components/Properties/Properties.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import { useStyles } from "./Properties.elements";
+import { TextField } from "@material-ui/core";
 const Properties = () => {
 	const classes = useStyles();
 	return (
-		<div className={classes.properties}>
-			<h2>Properties</h2>
-		</div>
+		<form className={classes.properties} noValidate autoComplete="off">
+			<TextField
+				className={classes.name}
+				label="Name"
+				id="outlined-size-small"
+				defaultValue=" "
+				variant="outlined"
+				size="small"
+			/>
+			<TextField
+				className={classes.description}
+				label="Description"
+				id="outlined-size-small"
+				defaultValue=" "
+				variant="outlined"
+				size="small"
+				multiline
+				rows={3}
+			/>
+		</form>
 	);
 };
 
