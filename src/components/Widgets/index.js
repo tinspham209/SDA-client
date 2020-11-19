1025042a261f4436ebaec74e211311701b681b2b
tinspham209 <tindeptrai1@gmail.com>
2020-11-19 10:22:35 +0700

    add: Industry component

--- src/components/Widgets/index.js
+++ src/components/Widgets/index.js
@@ -1,4 +1,5 @@
 export { default as Humidity } from "./Climate/Humidity/Humidity";
+export { default as Industry } from "./Industry/IndustryProduction";
 export { default as LineChart } from "./Visualization/LineChart/LineChart";
 export { default as ColumnChart } from "./Visualization/ColumnChart/ColumnChart";
 export { default as Maps } from "./Visualization/Maps/Maps";
