6bc8ccbbb2784b071a877e5a4d5531d292843a3f
tinspham209 <tindeptrai1@gmail.com>
2020-11-19 10:05:08 +0700

    add: Maps component

---
+++ src/components/Widgets/Visualization/Maps/Maps.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+import { useStyles } from "./Maps.elements";
+import { IconButton, Button } from "@material-ui/core";
+
+import { AiFillCloseCircle } from "react-icons/ai";
+import { RiErrorWarningFill } from "react-icons/ri";
+
+import Maps from "../../../Visualization/Maps/Maps";
+
+import { useDispatch } from "react-redux";
+import { setInfoWidget } from "../../../../app/slice/dashboardSlice";
+
+const WidgetLineChart = ({ id, data, inputs, outputs }) => {
+	const classes = useStyles();
+	const dispatch = useDispatch();
+
+	const handleOnClick = () => {
+		console.log("handleOnClick Maps");
+	};
+
+	const handleQuestionButton = (id) => {
+		id = id.split("-")[0];
+		const action = setInfoWidget(id);
+		dispatch(action);
+	};
+
+	return (
+		<div className={classes.node}>
+			<div className={classes.header}>
+				<div className={classes.headerLeft} />
+				<div className={classes.headerCenter}>
+					<p className={classes.headerTitle}>Maps</p>
+				</div>
+				<div className={classes.headerRight}>
+					<IconButton
+						aria-label="delete"
+						size="small"
+						color="inherit"
+						onClick={() => handleQuestionButton(id)}
+					>
+						<RiErrorWarningFill />
+					</IconButton>
+					<IconButton
+						aria-label="delete"
+						size="small"
+						color="inherit"
+						onClick={() => data.onClick(id)}
+					>
+						<AiFillCloseCircle />
+					</IconButton>
+				</div>
+			</div>
+			<div className={classes.button}>
+				<Button
+					size="small"
+					variant="contained"
+					color="primary"
+					onClick={() => handleOnClick()}
+				>
+					Run
+				</Button>
+			</div>
+			<div className={classes.body}>
+				<div className={classes.portIn}>
+					{inputs.map((port) =>
+						React.cloneElement(port, {
+							style: {
+								width: "15px",
+								height: "15px",
+								background: "#1B263B",
+								borderRadius: "50%",
+							},
+						})
+					)}
+				</div>
+				<Maps />
+			</div>
+		</div>
+	);
+};
+
+export default WidgetLineChart;
