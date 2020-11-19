bc5435f019f64da6cc8169b8bec91a3ef8db15a1
tinspham209 <tindeptrai1@gmail.com>
2020-11-20 00:12:22 +0700

    update: Maps component

--- src/components/Visualization/Maps/Maps.elements.js
+++ src/components/Visualization/Maps/Maps.elements.js
@@ -2,7 +2,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles((theme) => ({
 	maps: {
-		width: "300px",
+		width: "350px",
 		height: "100%",
 	},
 }));
--- src/components/Visualization/Maps/Maps.jsx
+++ src/components/Visualization/Maps/Maps.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
@@ -6,83 +6,21 @@ import highchartsMap from "highcharts/modules/map";
 import { vnAll } from "./vnAll";
 
 import { useStyles } from "./Maps.elements";
+import { useSelector } from "react-redux";
 
 highchartsMap(Highcharts); // Initialize the map module
 
 const Maps = () => {
 	const classes = useStyles();
 
-	const [dataMaps, setDataMaps] = useState([
-		["vn-3655", 0], //viet nam
-		["vn-qn", 92], //quang ninh
-		["vn-kh", 106.8], //khanh hoa
-		["vn-tg", 118.9], //tien giang
-		["vn-bv", 99.5], //ba ria - vung tau
-		["vn-bu", 107.8], //binh thuan
-		["vn-hc", 105], //tp.ho chi minh
-		["vn-br", 114.8], //ben tre
-		["vn-st", 102], //soc trang
-		["vn-pt", 97.5], //phu tho
-		["vn-yb", 114.5], //yen bai
-		["vn-hd", 99], //hai duong
-		["vn-bn", 175], //bac ninh
-		["vn-317", 108.9], //hung yen
-		["vn-nb", 111.6], //ninh binh
-		["vn-hm", 130.7], //ha nam
-		["vn-ho", 128.7], //hoa binh
-		["vn-vc", 97], //vinh phuc
-		["vn-318", 105], //ha noi
-		["vn-bg", 122.9], //bac giang
-		["vn-tb", 106.5], //thai binh
-		["vn-ld", 126], //lam dong
-		["vn-bp", 114.7], //binh phuoc
-		["vn-py", 113.4], //phu yen
-		["vn-bd", 106.8], //binh dinh
-		["vn-724", 121], //gia lai
-		["vn-qg", 107.7], //quang ngai
-		["vn-331", 107.4], //dong nai
-		["vn-dt", 105.2], //dong thap
-		["vn-la", 114.6], //long an
-		["vn-3623", 103.9], //hai phong
-		["vn-337", 135.4], //hau giang
-		["vn-bl", 103.8], //bac lieu
-		["vn-vl", 102.1], //vinh long
-		["vn-tn", 121.4], //tay ninh
-		["vn-ty", 106.7], //thai nguyen
-		["vn-li", 101.2], //lai chau
-		["vn-311", 117], //son la
-		["vn-hg", 118.6], //ha giang
-		["vn-nd", 116.4], //nam dinh
-		["vn-328", 107], //ha tinh
-		["vn-na", 109.7], //nghe an
-		["vn-qb", 108.5], //quang binh
-		["vn-723", 110.5], //dak lak
-		["vn-nt", 110.9], //ninh thuan
-		["vn-6365", 109.4], //dak nong
-		["vn-299", 115.3], //kon tum
-		["vn-300", 118.2], //quang nam
-		["vn-qt", 113], //quang tri
-		["vn-tt", 112.6], //thua thien hue
-		["vn-da", 106], //da nang
-		["vn-ag", 106.6], //an giang
-		["vn-cm", 109.9], //ca mau
-		["vn-tv", 116.2], //tra vinh
-		["vn-cb", 99.1], //cao bang
-		["vn-kg", 105.8], //kien giang
-		["vn-lo", 104], //lao cai
-		["vn-db", 105.4], //dien bien
-		["vn-ls", 96.1], //lang son
-		["vn-th", 107.9], //thanh hoa
-		["vn-307", 74.9], //bac kan
-		["vn-tq", 120.8], //tuyen quang
-		["vn-bi", 110.4], //binh duong
-		["vn-333", 104.6], //can tho
-	]);
+	const dataMaps = useSelector((state) => state.dashboard.viz.maps.data);
+	const dataTitleMaps = useSelector((state) => state.dashboard.viz.maps.title);
 
 	const [mapsOptions, setMapsOptions] = useState({
 		title: {
-			text: "Industry of VN 2012",
+			text: ``,
 		},
+
 		tooltip: {
 			headerFormat: "{point.name}",
 			pointFormat: `{point.name}, {point.value}`,
@@ -97,16 +35,29 @@ const Maps = () => {
 			},
 		},
 		colorAxis: {
-			min: 50,
-			minColor: "#E6E7E8",
-			maxColor: "#005645",
+			min: 0,
+			minColor: "rgb(197, 197, 115)",
+			maxColor: "rgb(247, 2, 2)",
+			dataClasses: [
+				{
+					to: 80,
+				},
+				{ from: 80, to: 100 },
+				{ from: 100, to: 110 },
+				{ from: 110, to: 120 },
+				{ from: 120, to: 130 },
+				{ from: 130 },
+			],
 		},
 		legend: {
 			layout: "vertical",
 			align: "right",
-			verticalAlign: "middle",
+			verticalAlign: "bottom",
+			margin: 5,
+			floating: true,
+			backgroundColor: "rgba(255,255,255,0.5)",
+			borderRadius: 5,
 		},
-
 		subtitle: {
 			text: "",
 			floating: true,
@@ -116,10 +67,16 @@ const Maps = () => {
 				fontSize: "16px",
 			},
 		},
+		mapNavigation: {
+			enabled: true,
+			buttonOptions: {
+				verticalAlign: "bottom",
+			},
+		},
 		series: [
 			{
 				mapData: vnAll,
-				data: dataMaps,
+				data: [],
 				name: "VN",
 				dataLabels: {
 					enabled: true,
@@ -127,13 +84,31 @@ const Maps = () => {
 				},
 			},
 		],
-		mapNavigation: {
-			enabled: true,
-			buttonOptions: {
-				verticalAlign: "bottom",
-			},
-		},
 	});
+
+	useEffect(() => {
+		let dataMap = [];
+		dataMaps.map((item) => {
+			dataMap.push({
+				"hc-key": item[0],
+				value: item[1],
+			});
+			return null;
+		});
+		setMapsOptions({
+			...mapsOptions,
+			title: {
+				text: `Industry of VN ${dataTitleMaps}`,
+			},
+			series: [
+				{
+					...mapsOptions.series[0],
+					data: dataMap,
+				},
+			],
+		});
+		// eslint-disable-next-line
+	}, [dataMaps, dataTitleMaps]);
 
 	return (
 		<div className={classes.maps}>
