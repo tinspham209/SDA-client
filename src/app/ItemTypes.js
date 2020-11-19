7c7be40aa008f5615d9a4e17681973e563b6a573
tinspham209 <tindeptrai1@gmail.com>
2020-11-19 18:13:12 +0700

    add: CLIMATE_TEMPERATURE

--- src/app/ItemTypes.js
+++ src/app/ItemTypes.js
@@ -5,6 +5,7 @@ export const DASHBOARD = "DASHBOARD";
 export const TOOLBAR = "TOOLBAR";
 
 export const CLIMATE_HUMIDITY = "avgHumidity";
+export const CLIMATE_TEMPERATURE = "avgTemperature";
 export const INDUSTRY_PRODUCTION = "industryItem";
 export const VIZ_LINECHART = "lineChart";
 export const VIZ_COLUMNCHART = "columnChart";
