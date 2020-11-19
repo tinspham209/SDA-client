6b8eeff970c90df24c2b2b8d87c8fa6e67448dfc
tinspham209 <tindeptrai1@gmail.com>
2020-11-19 10:05:46 +0700

    add: Industry

---
+++ src/api/vnId.js
@@ -0,0 +1,34 @@
+export const vn = {
+	vietnam: {
+		id: "vn-3655",
+		name: "Việt Nam",
+	},
+	bentre: {
+		id: "vn-br",
+		name: "Bến tre",
+	},
+	baria: {
+		id: "vn-bv",
+		name: "Bà Rịa - Vũng Tàu",
+	},
+	bacninh: {
+		id: "vn-bn",
+		name: "Bắc Ninh",
+	},
+	angiang: {
+		id: "vn-ag",
+		name: "An Giang",
+	},
+	bacgiang: {
+		id: "vn-bg",
+		name: "Bắc Giang",
+	},
+	backan: {
+		id: "vn-307",
+		name: "Bắc Kạn",
+	},
+	baclieu: {
+		id: "vn-bl",
+		name: "Bạc Liêu",
+	},
+};
