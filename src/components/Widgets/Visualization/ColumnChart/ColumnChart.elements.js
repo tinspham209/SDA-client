d897a970c953a217cd47195aa52af9f85fea3694
tinspham209 <tindeptrai1@gmail.com>
2020-11-27 13:03:45 +0700

    update UI

--- src/components/Widgets/Visualization/ColumnChart/ColumnChart.elements.js
+++ src/components/Widgets/Visualization/ColumnChart/ColumnChart.elements.js
@@ -28,6 +28,7 @@ export const useStyles = makeStyles((theme) => ({
 		border: "1px solid #000",
 		margin: "10px",
 		overflowY: "auto",
+		minHeight: "100px",
 	},
 	portIn: {
 		position: "absolute",
